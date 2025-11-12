c14f195c8bb98b711608a504c74e3e36f0acee66
Daniel <danielelacer08@gmail.com>
2025-11-12 15:50:23 +0000

    Update examples

--- example.sh
+++ example.sh
@@ -1,19 +1,7 @@
-chmod 755 *.sh
-conda create -y -p ./llama_server_env python=3.10
-conda activate ./llama_server_env
+bash ./build.sh
 
-#these may be pre-installed, or available as modules
-conda install -y anaconda::git
-conda install -y -c conda-forge gxx
-
-pip install -r requirements.txt
-git clone https://github.com/ggerganov/llama.cpp
-cd llama.cpp
-make
-cd ..
-
-python get_model.py nvidia/Llama3-ChatQA-1.5-8B
-#Using 8-bit quantized model for testing
-./start.sh nvidia/Llama3-ChatQA-1.5-8B_Q8_0 &
+# for other models check https://ollama.com/library
+./activate_conda_env.sh python get_model.py llama3-chatqa:8b
+./start.sh llama3-chatqa:8b &
 ./prompt.sh "Tell me a joke about a computer scientist who couldn't write a shell script."
 ./exit.sh
--- slexample.sh
+++ slexample.sh
@@ -2,28 +2,10 @@
 # log into huggingface  account with the following command: huggingface-cli login
 # (after installing huggingface-hub)
 
-#Install miniconda into your local user
-#Download the latest miniconda installer from https://docs.conda.io/en/latest/miniconda.html
-#Run the installer
-chmod 755 *.sh
-conda create -y -p ./llama_server_env python=3.10
-conda activate ./llama_server_env
-#these may be pre-installed, or available as modules
+bash ./build.sh
 
-conda install -y anaconda::git
-conda install -y -c conda-forge gxx
-
-
-pip install -r requirements.txt
-git clone https://github.com/ggerganov/llama.cpp
-cd llama.cpp
-make
-cd ..
-
-python get_model.py nvidia/Llama3-ChatQA-1.5-8B
-#Using 8-bit quantized model for testing
-sbatch ./slurm_start.sh nvidia/Llama3-ChatQA-1.5-8B_Q8_0
-#./start.sh nvidia/Llama3-ChatQA-1.5-8B &
+# for other models check https://ollama.com/library
+./activate_conda_env.sh python get_model.py llama3-chatqa:8b
+sbatch ./slurm_start.sh llama3-chatqa:8b
 ./prompt.sh "Tell me a joke about a computer scientist who couldn't write a shell script."
 ./exit.sh
-
